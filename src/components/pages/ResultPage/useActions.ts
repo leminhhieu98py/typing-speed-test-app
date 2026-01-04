302741b9e2cca40c91433aa4dc4a6d1d25813a7b
leminhhieu98py <leminhhieu98py@gmail.com>
2026-01-04 18:49:06 +0700

    feat: replace the search params for better UX

--- src/components/pages/ResultPage/useActions.ts
+++ src/components/pages/ResultPage/useActions.ts
@@ -1,16 +1,21 @@
 import { fireFireworks } from '@/utils/confettiUtils';
-import { useSearch } from '@tanstack/react-router';
+import { useNavigate, useSearch } from '@tanstack/react-router';
 
 import { useEffect } from 'react';
 
 export const useActions = () => {
   const { from }: { from?: string } = useSearch({ from: '/result' });
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (from === 'homepage') {
       fireFireworks();
+      navigate({
+        to: '/result',
+        replace: true,
+      });
     }
-  }, [from]);
+  }, [from, navigate]);
 
   return {};
 };
